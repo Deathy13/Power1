9ede26b0d9f0e703d3940418a0deea5e0efcc447
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-08-31 23:09:26 +1000

    dialogue done

---
+++ Assets/Scripts/Daialogue/DialugoeTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialugoeTrigger : MonoBehaviour
+{
+    public Dialugoe dialugoe;
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialugoe);
+    }
+}
